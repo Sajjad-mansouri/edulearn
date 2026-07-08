81e32860980f02658087aab1d770edbbb592fc97
Sajjad Mansouri <s.mansouri4w@gmail.com>
2026-07-09 00:23:50 +0330

    implement custom user model using AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("Email Address"), blank=True, unique=True)
+
+    REQUIRED_FIELDS = ["email"]
+    email_verified = models.BooleanField(_("Email verified"), default=False)
+
+    class Meta(AbstractUser.Meta):
+        indexes = [models.Index(fields=["username"])]
