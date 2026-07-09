f4ce109675dab8e8702269d8dde13468081eb2d4
Sajjad Mansouri <s.mansouri4w@gmail.com>
2026-07-09 10:55:21 +0330

    implement UserSession model

--- accounts/models.py
+++ accounts/models.py
@@ -29,3 +29,27 @@ class Role(models.Model):
 
     def __str__(self):
         return f"{self.get_name_display()}"
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sessions", verbose_name=_("User")
+    )
+    device = models.CharField(_("Device"), max_length=255)
+    ip_address = models.GenericIPAddressField(_("IP Address"))
+    user_agent = models.TextField(_("User Agent"))
+
+    login_at = models.DateTimeField(auto_now_add=True)
+    last_activity_at = models.DateTimeField(auto_now=True)
+
+    is_revoked = models.BooleanField(_("Is Revoked"), default=False)
+
+    class Meta:
+        ordering = ["-login_at"]
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["last_activity_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.device}"
