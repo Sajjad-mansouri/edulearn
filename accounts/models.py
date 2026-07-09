6113ed78de92965c4e4078ceb66c0126abb01bd3
Sajjad Mansouri <s.mansouri4w@gmail.com>
2026-07-09 10:26:13 +0330

    Create Role model

--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,18 @@ class User(AbstractUser):
 
     class Meta(AbstractUser.Meta):
         indexes = [models.Index(fields=["username"])]
+
+
+class Role(models.Model):
+    ROLE_CHOICES = [
+        ("guest", "Guest"),
+        ("student", "Student"),
+        ("instructor", "Instructor"),
+        ("teaching assistant", "Teaching Assistant"),
+        ("moderator", "Moderator"),
+        ("support agent", "Support Agent"),
+        ("admin", "Admin"),
+    ]
+    user = models.ManyToManyField(User, related_name="roles", verbose_name=_("User"))
+    name = models.CharField(_("Name"), choices=ROLE_CHOICES, max_length=18)
+    description = models.TextField(_("Description"))
