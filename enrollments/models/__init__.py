cb936f197c457ceb6a64ad84203b780b3b2808b4
Sajjad Mansouri <s.mansouri4w@gmail.com>
2026-07-24 12:45:38 +0330

    feat(enrollments): implement LessonProgress model

--- enrollments/models/__init__.py
+++ enrollments/models/__init__.py
@@ -1,4 +1,5 @@
 from .enrollment import Enrollment
 from .lesson_content_progress import LessonContentProgress
+from .lesson_progress import LessonProgress
 
-__all__ = ["Enrollment", "LessonContentProgress"]
+__all__ = ["Enrollment", "LessonContentProgress", "LessonProgress"]
