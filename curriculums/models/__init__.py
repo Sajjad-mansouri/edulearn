4437b6ceb77c0bdb98db80026d5eb332753f23a9
Sajjad Mansouri <s.mansouri4w@gmail.com>
2026-07-24 10:20:13 +0330

    feat(curriculums): implement QuizContent model

--- curriculums/models/__init__.py
+++ curriculums/models/__init__.py
@@ -2,6 +2,7 @@ from .article_content import ArticleContent
 from .file_content import FileContent
 from .lesson import Lesson
 from .lesson_content import LessonContent
+from .quiz_content import QuizContent
 from .section import Section
 from .video_content import VideoContent
 
@@ -12,4 +13,5 @@ __all__ = [
     "VideoContent",
     "FileContent",
     "ArticleContent",
+    "QuizContent",
 ]
